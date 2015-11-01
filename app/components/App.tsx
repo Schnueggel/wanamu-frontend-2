b6681888b2f93f85643656ae47426ee0e7d4b3fd
Christian Steinmann <christian.steinmann77@gmail.com>
2015-11-01 17:44:03 +0100

    Initial commit

---
+++ app/components/App.tsx
@@ -0,0 +1,13 @@
+/// <reference path="../../typings/tsd.d.ts" />
+import React from 'react';
+
+class App extends React.Component {
+    constructor(props:Object){
+        super(props);
+    }
+    render() {
+        return (
+            <div>Wanamu App</div>
+        );
+    }
+}
