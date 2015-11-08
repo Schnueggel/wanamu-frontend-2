cf0cb94ba53556d65bacf457dace949abad49cc9
Christian Steinmann <christian.steinmann77@gmail.com>
2015-11-08 16:54:09 +0100

    Add moment and lodash module

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -3,3 +3,6 @@
 /// <reference path="react-dom/react-dom.d.ts" />
 /// <reference path="react-router-1.0.0rc/react-router.d.ts" />
 /// <reference path="q/Q.d.ts" />
+/// <reference path="moment/moment-node.d.ts" />
+/// <reference path="moment/moment.d.ts" />
+/// <reference path="lodash/lodash.d.ts" />
