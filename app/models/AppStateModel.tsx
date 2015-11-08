777b532dee2de7ce3150716150830f4549d5e1ef
Christian Steinmann <christian.steinmann77@gmail.com>
2015-11-08 23:51:40 +0100

    Remove business logic

--- app/models/AppStateModel.tsx
+++ app/models/AppStateModel.tsx
@@ -3,47 +3,27 @@ import * as Rx from 'rx';
 import * as Q from 'q';
 
 export interface LoginState {
-    isEmailValid : boolean,
+    errorMessage: string;
+    email : string,
     emailErrors : Array<string>,
-    isPasswordValid : boolean,
-    isFormValid : boolean,
     passwordErrors : Array<string>
 }
 
 export class AppStateModel {
     menu: any;
+
     login: LoginState = {
-        isEmailValid : false,
-        emailErrors : [],
-        isPasswordValid : false,
-        isFormValid : false,
-        passwordErrors : []
-    };
+        errorMessage: '',
+        email: '',
+        emailErrors: [],
+        passwordErrors: []
+    } as LoginState;
 
     loginStateChangedStream: Rx.Subject<LoginState>;
 
     constructor() {
         this.menu = new MenuModel();
         this.loginStateChangedStream = new Rx.Subject<LoginState>();
-    }
-
-    /**
-     * This code is here for testing purpose it should be moved to a LoginService
-     * @param observable
-     * @returns {Observable<TResult>}
-     */
-    createLoginResponseStream(observable: Rx.Observable<{email:string,password:string}>) : Rx.Observable<boolean> {
-        return observable
-            .flatMap((data:{email:string,password:string}) => {
-                return Rx.Observable.fromPromise(Q.fcall<boolean>(() => {
-                    console.log('Do Login');
-                    return true;
-                }));
-            })
-            .map((result:boolean) => {
-                console.log('Login done:' + result);
-                return result;
-            });
     }
 
     notifyLoginState() {
