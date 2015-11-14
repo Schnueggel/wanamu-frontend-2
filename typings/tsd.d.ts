5313c35afe2f2a22ebb1d0e7aea5eee3a413b6bd
Christian Steinmann <christian.steinmann77@gmail.com>
2015-11-14 01:15:36 +0100

    Add Es6 Promise tsd

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -6,3 +6,4 @@
 /// <reference path="moment/moment-node.d.ts" />
 /// <reference path="moment/moment.d.ts" />
 /// <reference path="lodash/lodash.d.ts" />
+/// <reference path="es6-promise/es6-promise.d.ts" />
