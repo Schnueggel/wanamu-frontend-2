2a4b00ceeafb19fe3c19cc07da8c876e8bde23ca
Christian Steinmann <christian.steinmann77@gmail.com>
2015-11-14 23:38:00 +0100

    Add User Model creation on login

--- app/services/AuthService.ts
+++ app/services/AuthService.ts
@@ -10,14 +10,13 @@ export interface LoginRequestData {
 }
 
 export interface LoginResponse extends axios.Response {
-    data: wu.model.data.IUserData;
+    data: {data: Array<wu.model.data.IUserData>};
 }
 
 export class AuthService {
 
-    private loginRequestStream: Rx.Observable<wu.model.data.IUser>;
     private axios:axios.AxiosInstance;
-    private currentUser: wu.model.data.IUser;
+    private currentUser:wu.model.data.IUser;
 
     constructor() {
         this.createAxios();
@@ -33,10 +32,10 @@ export class AuthService {
      * @param response
      * @returns {any}
      */
-    onResponseError(response: axios.Response) {
+    onResponseError(response:axios.Response) {
         if (response.status === 401 || response.status === 403) {
             return Promise.resolve(new Err.CredentialsError());
-        } else if ( response.status === 500) {
+        } else if (response.status === 500) {
             return Promise.resolve(new Err.ServerError());
         } else if (response.status === 0) {
             return Promise.resolve(new Err.NetworkError());
@@ -45,32 +44,31 @@ export class AuthService {
             return Promise.resolve(new Err.UnknownError('Server responded with: ' + response.statusText));
         }
     }
+
     /**
      *
      * @param obs
      * @returns {Rx.Observable<boolean>}
-     * @throws NetworkError|UnknownError|ServerError|CredentialsError
+     * @throws NetworkError|UnknownError|ServerError|CredentialsError|InvalidResponseDataError
      */
-    createLoginRequestStream(obs:Rx.Observable<LoginRequestData>): Rx.Observable<wu.model.data.IUser>{
-        if (this.loginRequestStream === undefined) {
-            this.loginRequestStream = obs
-                .flatMapLatest((data: LoginRequestData) => this.getLoginStream('http://localhost:3001/auth/login', data))
-                .catch((e: Error) => {
-                    console.error(e);
-                    return Rx.Observable.just(new Err.UnknownError('An unknown error happened'));
-                })
-                .map((response: LoginResponse) => {
-                    if (response instanceof Err.BaseError) {
-                        console.error(response);
-                        return response as any;
-                    } else {
-                        this.currentUser = new User(response.data);
-                        return this.currentUser;
-                    }
-                });
-        }
-
-        return this.loginRequestStream;
+    createLoginRequestStream(obs:Rx.Observable<LoginRequestData>):Rx.Observable<wu.model.data.IUser> {
+        return obs
+            .flatMapLatest((data:LoginRequestData) => this.getLoginStream('http://localhost:3001/auth/login', data))
+            .catch((e:Error) => {
+                console.error(e);
+                return Rx.Observable.just(new Err.UnknownError('An unknown error happened'));
+            })
+            .map((response:LoginResponse) => {
+                if (response instanceof Err.BaseError) {
+                    console.error(response);
+                    return response as any;
+                } else if (_.get(response, '.data.data[0].id', false) === false){
+                    return new Err.InvalidResponseDataError();
+                } else {
+                    this.currentUser = new User(response.data.data[0]);
+                    return this.currentUser;
+                }
+            });
     }
 
     /**
@@ -80,11 +78,12 @@ export class AuthService {
      * @returns {Rx.Observable<axios.Response>}
      * @throws NetworkError|UnknownError|ServerError|CredentialsError
      */
-    getLoginStream(url: string, data: LoginRequestData): Rx.Observable<axios.Response>  {
+    getLoginStream(url:string, data:LoginRequestData):Rx.Observable<axios.Response> {
         return Rx.Observable
             .fromPromise(this.axios.post(url, data));
     }
 }
+
 const authService = new AuthService();
 
 export default authService;
