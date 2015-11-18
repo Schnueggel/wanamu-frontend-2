15b57d2fba6eed7c6225be6991facbb3a66ab234
Christian Steinmann <christian.steinmann77@gmail.com>
2015-11-18 23:21:07 +0100

    Check for user on page load

--- app/components/Page.tsx
+++ app/components/Page.tsx
@@ -1,31 +1,54 @@
 import * as React from 'react';
 import Menu from './Menu/Menu';
-import AppState from '../models/state/AppStateModel';
+import AppState, {AppStateModel} from '../models/state/AppStateModel';
+import authService from '../services/AuthService';
+import {User} from '../models/data/User';
 
-class PageProps {
+interface PageProps {
     children: any
 }
 
-export default class Page extends React.Component<PageProps, any> {
+export default class Page extends React.Component<PageProps, AppStateModel> {
 
-    constructor(props:PageProps){
+    private currentUserLoaded:boolean = false;
+
+    constructor(props:PageProps) {
         super(props);
         this.state = AppState;
     }
 
-    render(){
-        return (
-            <div>
+    componentWillMount() {
+        this.state.triedToLoadUser = false;
+        authService
+            .createCurrentUserRequestStream()
+            .finally(() => {
+                this.setState({
+                    triedToLoadUser: true
+                } as any);
+            })
+            .subscribe((user:wu.model.data.IUser) => {
+                if (user instanceof User) {
+                    this.state.login.user = user;
+                }
+            });
+    }
+
+    render() {
+        if (this.state.triedToLoadUser) {
+            return (
                 <div>
-                    <h1>Wanamu Page!!!</h1>
+                    <div>
+                        <h1>Wanamu Page!!!</h1>
+                    </div>
+                    <nav>
+                        <Menu {...this.state.menu}/>
+                    </nav>
+                    <div className="container">
+                        {this.props.children}
+                    </div>
                 </div>
-                <nav>
-                    <Menu {...this.state.menu}/>
-                </nav>
-                <div className="container">
-                    {this.props.children}
-                </div>
-            </div>
-        );
+            );
+        }
+        return null;
     }
 }
