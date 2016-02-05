1cc63a89d3ad5029ef3affcd5b7ea81430e43702
Christian Steinmann <christian.steinmann77@gmail.com>
2016-02-06 00:50:50 +0100

    Add tslint

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,21 @@ module.exports = {
     },
     plugins: [],
     module: {
-        npParse: /node_modules/,
+        noParse: [
+            /[/\\]react[/\\]/,
+            /node_modules[/\\]redux/,
+            /[/\\]rx[/\\]/,
+            /[/\\]webpack[/\\]/,
+            /[/\\]webpack-dev-server[/\\]/,
+            /[/\\]react-router[/\\]/,
+            /[/\\]react-redux[/\\]/
+        ],
+        preLoaders: [
+            {
+                test: /\.tsx?$/,
+                loader: "tslint-loader"
+            }
+        ],
         loaders: [
             {
                 test: /\.tsx?$/,
@@ -34,5 +48,21 @@ module.exports = {
     },
     postcss: function () {
         return [postcssNesting, cssnext, autoprefixer];
+    },
+    tslint: {
+        configuration: {
+            rules: {
+                quotemark: [true, "single"]
+            }
+        },
+
+        // tslint errors are displayed by default as warnings
+        // set emitErrors to true to display them as errors
+        emitErrors: true,
+
+        // tslint does not interrupt the compilation by default
+        // if you want any file with tslint errors to fail
+        // set failOnHint to true
+        failOnHint: true
     }
 };
