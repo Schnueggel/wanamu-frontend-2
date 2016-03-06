50133a0b8202a32d1f57cafd66cd495209f38e28
Christian Steinmann <christian.steinmann77@gmail.com>
2016-03-06 01:02:55 +0100

    Update to new store handling of react-redux-router

--- app/stores/appStore.ts
+++ app/stores/appStore.ts
@@ -1,18 +1,14 @@
 import { createStore, applyMiddleware } from 'redux';
 import { browserHistory } from 'react-router'
-import { syncHistory } from 'react-router-redux';
+import { syncHistoryWithStore, routerMiddleware } from 'react-router-redux';
 import rootReducer from '../reducers';
 import * as thunkMiddleware from 'redux-thunk';
 import * as createLogger from 'redux-logger';
 
 const logger = (createLogger as any)() ;
-const reduxRouterMiddleware = syncHistory(browserHistory);
 
-const createStoreWithMiddleware = applyMiddleware(reduxRouterMiddleware, thunkMiddleware as any, logger)(createStore);
+const createStoreWithMiddleware = applyMiddleware(routerMiddleware(browserHistory), thunkMiddleware as any, logger)(createStore);
 
 const store = createStoreWithMiddleware(rootReducer);
-
-// Required for replaying actions from devtools to work
-reduxRouterMiddleware.listenForReplays(store);
 
 export default store;
