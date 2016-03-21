9e13522fa2a3fe70c95020efa55c051c9dc5a01e
Christian Steinmann <christian.steinmann77@gmail.com>
2016-03-21 23:23:39 +0100

    Add production build code

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const postcssNesting = require('postcss-nesting'),
+    webpack = require('webpack'),
     cssnext = require('postcss-cssnext');
 
 module.exports = {
@@ -16,7 +17,13 @@ module.exports = {
         // Add `.ts` and `.tsx` as a resolvable extension.
         extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js', '.d.ts']
     },
-    plugins: [],
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        })
+    ],
     module: {
         noParse: [],
         preLoaders: [
